3221d47a58a35786a053a3e844314141ac8c20ab
Dantin Durand <durand.dantin.pro@gmail.com>
2021-06-11 02:19:33 +0200

    modification du manifest

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -9,33 +9,27 @@ if (process.env.NODE_ENV === "production") {
         "App is being served from cache by a service worker.\n" +
           "For more details, visit https://goo.gl/AFskqB"
       );
-      window.alert("ready");
     },
     registered() {
       console.log("Service worker has been registered.");
-      window.alert("registered");
     },
     cached() {
       console.log("Content has been cached for offline use.");
-      window.alert("cached");
     },
     updatefound() {
       console.log("New content is downloading.");
-      window.alert("updatefound");
     },
     updated() {
       console.log("New content is available; please refresh.");
-      window.alert("updated");
     },
+
     offline() {
       console.log(
         "No internet connection found. App is running in offline mode."
       );
-      window.alert("offline");
     },
     error(error) {
       console.error("Error during service worker registration:", error);
-      window.alert(error);
     },
   });
 }
